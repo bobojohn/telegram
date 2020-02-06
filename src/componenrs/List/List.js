7d2ffaafef3143a4c51724122ebbc2e13cb61eed
bobo <bobojon_96.96@mail.ru>
2020-02-06 11:28:26 +0500

    telegram

---
+++ src/componenrs/List/List.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function List(props) {
+    const online = props.online ? 'online' : ''
+
+
+    return (
+        <div className="main">
+            <div className="cont">
+                <div className="img">
+                    <img src={props.imgUrl} />
+                    <span className={online}>{props.online}</span>
+                </div>
+                <div className="nameAndLastMaseg">
+                    <h6>{props.name}</h6>
+                    <span>{props.lastMessage}</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+
